a3881455830b0fc254c8f62543828287a0c6c197
CubeFlix <55668748+CubeFlix@users.noreply.github.com>
2020-06-13 10:19:50 -0500

    lets begin :)

---
+++ baazarAutoTrader.py
@@ -0,0 +1,16 @@
+# An auto-trader for Minecraft Hypixel Skyblock's bazaar.
+#
+# This is bannable (i think), so please don't do this on the server. This was just for a programming challange.
+
+import requests
+import pyautogui as p
+
+itemTrading = "BROWN_MUSHROOM"
+maxTransactions = 10
+currentTransactions = 0
+
+
+
+while 
+
+
