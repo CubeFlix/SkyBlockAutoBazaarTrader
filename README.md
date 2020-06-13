ccc4ac537670b5e20e541537d30243fc81560da1
CubeFlix <55668748+CubeFlix@users.noreply.github.com>
2020-06-13 12:39:11 -0500

    added stuff

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SkyBlockAutoBazaarTrader
+
+![The Bazaar](docimages/bazaar.PNG)
+
  A auto-trader for Hypixel Skyblock's bazaar.
+
+SkyBlock has a NPC named the Bazaar, and you can buy and sell items, and the price will fluctuate.
+
+This tool will automatically trade an item of your choice.
+
+Instructions: 
+============
+
+Download the script.
+
+Install pyautogui (pip install pyautogui) and python (https://www.python.org/). 
+
+Enter the GUI for the bazaar, and open up the item you want to buy, and it's trading options.
+
+Set script variable itemTrading to be the ID of the item to sell. 
+
+Begin the script.
+
+
+Dependencies
+============
+
+Python 2-3 (https://www.python.org/)
+
+PyAutoGui "pip install pyautogui" (https://pypi.org/project/PyAutoGUI/)
--- baazarAutoTrader.py
+++ baazarAutoTrader.py
@@ -1,25 +1,62 @@
+#!python3
 # An auto-trader for Minecraft Hypixel Skyblock's bazaar. It works as an autoclicker.
-# Enable your default texture pack.
+# Enable your default texture pack. Add a .png image to images/ to the icon of the item you want to trade with the name of the item you are trading.
 # This is bannable (i think), so please don't do this on the server. This was just for a programming challange.
+# By Kevin Chen
 
-import requests
+import requests, time
 import pyautogui as p
 
-itemTrading = "BROWN_MUSHROOM" # The item to trade
+itemTrading = "WHEAT" # The item to trade
 apiKey = '9be4bedf-a55a-4fcb-9155-969b9a1d47c2' # Your api key. Enter /api new on mc.hypixel.net.
 maxTransactions = 10
 currentTransactions = 0
+priceHistory = []
+
+class Transaction():
+    def __init__(self, ttype, amount):
+        self.ttype = ttype
+        self.amount = amount
 
 def _fetch(url):
     return (lambda u: requests.get(url).json())(url)
 
 def fetchPriceData():
     statdata = _fetch("https://api.hypixel.net/skyblock/bazaar?key=" + apiKey)
-    return {'sellPrice' : statdata['products'][item_name]['quick_status']['sellPrice'], 'buyPrice' : statdata['products'][item_name]['quick_status']['buyPrice']}
+    return {'sellPrice' : statdata['products'][itemTrading]['quick_status']['sellPrice'], 'buyPrice' : statdata['products'][itemTrading]['quick_status']['buyPrice']}
 
 def sell(amount):
-    p.
+    p.click('sell.png', button='right')
+    time.sleep(1)
+    for i in range(0, amount):
+        p.click(itemTrading+'.png')
+        time.sleep(1)
+    currentTransactions += 1
+        
+def buy(amount):
+    p.click('buy.png')
+    time.sleep(1)
+    for i in range(0, amount):
+        p.click(itemTrading+'.png')
+        time.sleep(1)
+    currentTransactions += 1
+    p.click('back.png')
 
-while 
+def calculateTranscation():
+    priceHistory.append(fetchPriceData())
+    return Transaction('none', 0)
+
+def trade():
+    while currentTransactions < maxTransactions:
+        trans = calculateTranscation()
+        if trans.ttype == 'buy':
+            buy(trans.amount)
+        elif trans.ttype == 'sell':
+            sell(trans.amount)
+        elif trans.ttype == 'none':
+            continue
+
+if __name__ == '__main__':
+    trade()
 
 
